a40c3783ad92f6070ffc8f4dc8a883a89688e7de
zayatihamza <125798481+zayatihamza@users.noreply.github.com>
2025-02-02 22:38:31 +0100

    Initial commit

---
+++ src/main/java/com/example/projet0/Projet0Application.java
@@ -0,0 +1,13 @@
+package com.example.projet0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projet0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projet0Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/projet0/entities/TypeMenu.java
@@ -0,0 +1,2 @@
+package com.example.projet0.entities;public enum TypeMenu {
+}
